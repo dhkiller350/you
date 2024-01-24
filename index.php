e90fe342a952cde504bd9a6a3ebf00755e91172a
admin505 <100965441+admin505admin505@users.noreply.github.com>
2024-01-24 16:11:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
 
+// set your e-mail address first, where you'll receive the notifications
+$yourEmailAddress = "user@domain.com";
+
+$emailSubject = "New Visitor on Webpage";
+$remoteIpAddress = $_SERVER['REMOTE_ADDR'];
+$emailContent = "Someone visited your webpage. IP address:".$remoteIpAddress;
+
+// send the message
+mail($yourEmailAddress, $emailSubject, $emailContent);
+
+?>
